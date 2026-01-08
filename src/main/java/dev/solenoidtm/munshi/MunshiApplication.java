01be131f661046bbbd1f5de27bbf4669393eed9f
solecore0 <raoabdullahn@gmail.com>
2026-01-08 23:44:54 +0500

    Initial Commit. Intialized Spring Boot Project. Munshi, The Accounting And ERP Software For Those With No Life

---
+++ src/main/java/dev/solenoidtm/munshi/MunshiApplication.java
@@ -0,0 +1,13 @@
+package dev.solenoidtm.munshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunshiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/solenoidtm/munshi/MunshiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.solenoidtm.munshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
